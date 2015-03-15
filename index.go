223dd6e531b8373b98a70f87043952f8a3e5f635
Tiago Natel de Moura <tiago4orion@gmail.com>
2015-03-15 17:32:32 -0300

    [CHANGE] improved docs

--- index.go
+++ index.go
@@ -1,30 +1,38 @@
-// Package neosearch is a full-text-search library focused in fast search on multiple
-// indices, doing data join between queries.
+// Package neosearch is a full-text-search library focused in fast search on
+// multiple indices, doing data join between queries.
+//
 // NeoSearch - Neoway Full Text Search Index
-// ==========================================
-// // NeoSearch is a feature-limited full-text-search library with focus on indices relationships, its main goal is provide very fast JOIN operations between information stored on different indices.  //
-// It's not a complete FTS (Full Text Search) engine, in the common sense, but aims to solve very specific problems of FTS. At the moment, NeoSearch is a laboratory for research, not recommended for production usage, here we will test various technology for fast storage and search algorithms. In the future, maybe, we can proud of a very nice tech for solve search in big data companies.
 //
-// NeoSearch is like a Lucene library but without all of the complexities of a complete FTS engine, written in Go, focusing on high performance search with data relationships.
+// NeoSearch is a feature-limited full-text-search library with focus on
+// indices relationships, its main goal is provide very fast JOIN
+// operations between information stored on different indices.
+// It's not a complete FTS (Full Text Search) engine, in the common sense, but
+// aims to solve very specific problems of FTS. At the moment, NeoSearch is a
+// laboratory for research, not recommended for production usage, here we will
+// test various technology for fast storage and search algorithms. In the
+// future, maybe, we can proud of a very nice tech for solve search in big
+// data companies.
 //
-// It's not yet complete, still in active development, then stay tuned for updates.
+// NeoSearch is like a Lucene library but without all of the complexities of
+// complete FTS engine, written in Go, focusing on high performance search
+// with data relationships.
 //
-// # Dependencies
+// It's not yet complete, still in active development, then stay tuned for
+// updates.
 //
-// * leveldb
-// * snappy (opcional, only required for compressed data)
-// * Go > 1.3
+// Dependencies
 //
-// # Install
+//     - leveldb
+//     - snappy (optional, only required for compressed data)
+//     - Go > 1.3
 //
-// ```bash
-// git clone git@bitbucket.org:i4k/neosearch.git
-// cd neosearch
-// go get -u -v .
-// go build -tags leveldb -v .
-// go test -tags leveldb -v .
-// ```
+// Install
 //
+//     git clone git@bitbucket.org:i4k/neosearch.git
+//     cd neosearch
+//     go get -u -v .
+//     go build -tags leveldb -v .
+//     go test -tags leveldb -v .
 //
 // Create and add documents to NeoSearch is very easy, see below:
 //
