321ce14cfe42856c1ad96848960c553db74f34f5
Tiago Natel de Moura <tiago4orion@gmail.com>
2015-03-22 13:35:29 -0300

    [CHANGE] change config data structure exportfing fields to support load configurations from config file

--- config.go
+++ config.go
@@ -1,5 +1,13 @@
 package neosearch
 
+import (
+	"io/ioutil"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
 // Option is a internal type used for setting config options.
 // This need be exported only because the usage of the
 // self-referential functions in the option assignments.
@@ -10,17 +18,17 @@ type Option func(c *Config) Option
 // Config stores NeoSearch configurations
 type Config struct {
 	// Root directory where all of the indices will be written.
-	dataDir string
+	DataDir string `yaml:"dataDir"`
 
 	// Enables debug in every sub-module
-	debug bool
+	Debug bool `yaml:"debug"`
 
 	// CacheSize is the length of LRU cache used by the storage engine
 	// Default is 1GB
-	cacheSize int
+	CacheSize int `yaml:"cacheSize"`
 
 	// EnableCache enable/disable cache support
-	enableCache bool
+	EnableCache bool `yaml:"enableCache"`
 }
 
 // NewConfig creates new config
@@ -40,8 +48,8 @@ func (c *Config) Option(opts ...Option) (previous Option) {
 // Debug enables or disable debug
 func Debug(t bool) Option {
 	return func(c *Config) Option {
-		previous := c.debug
-		c.debug = t
+		previous := c.Debug
+		c.Debug = t
 
 		return Debug(previous)
 	}
@@ -50,8 +58,8 @@ func Debug(t bool) Option {
 // EnableCache enables or disable index cache
 func EnableCache(t bool) Option {
 	return func(c *Config) Option {
-		previous := c.enableCache
-		c.enableCache = t
+		previous := c.EnableCache
+		c.EnableCache = t
 
 		return EnableCache(previous)
 	}
@@ -60,8 +68,8 @@ func EnableCache(t bool) Option {
 // DataDir set the data directory for neosearch database and indices
 func DataDir(path string) Option {
 	return func(c *Config) Option {
-		previous := c.dataDir
-		c.dataDir = path
+		previous := c.DataDir
+		c.DataDir = path
 
 		return DataDir(previous)
 	}
@@ -74,9 +82,29 @@ func DataDir(path string) Option {
 // http://htmlpreview.github.io/?https://github.com/google/leveldb/blob/master/doc/index.html
 func CacheSize(size int) Option {
 	return func(c *Config) Option {
-		previous := c.cacheSize
-		c.cacheSize = size
+		previous := c.CacheSize
+		c.CacheSize = size
 
 		return CacheSize(previous)
 	}
+}
+
+func ConfigFromFile(filename string) (*Config, error) {
+	// Load config from file
+	file, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatalf("Failed to open config file: %s", err.Error())
+	}
+
+	fileContent, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		log.Fatalf("Failed to read config file: %s", err.Error())
+	}
+
+	cfg := NewConfig()
+
+	err = yaml.Unmarshal(fileContent, &cfg)
+	return cfg, err
 }
