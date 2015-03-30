62b5e1114c03f72584bbde4b10271d5fd974b18d
Tiago Natel de Moura <tiago4orion@gmail.com>
2015-03-29 21:11:39 -0300

    [FEATURE] Improved build system

--- Makefile
+++ Makefile
@@ -1,11 +1,22 @@
 .PHONY: all build check shell
 
+DOCKER_DEVIMAGE = neosearch-dev-env
+DOCKER_PATH = /go/src/github.com/NeowayLabs/neosearch
+
 build:
 	docker build -t neosearch-dev-env .
 
 all: build
 	@-docker rm -vf neosearch-ctn
-	docker run --name neosearch-ctn -v `pwd`:/go/src/github.com/NeowayLabs/neosearch -i -t neosearch-dev-env hack/make.sh
+	docker run --name neosearch-ctn -v `pwd`:$(DOCKER_PATH) -i -t $(DOCKER_DEVIMAGE) hack/make.sh server cli
+
+server: build
+	@-docker rm -vf neosearch-ctn
+	docker run --name neosearch-ctn -v `pwd`:$(DOCKER_PATH) -i -t $(DOCKER_DEVIMAGE) hack/make.sh server
+
+cli: build
+	@-docker rm -vf neosearch-ctn
+	docker run --name neosearch-ctn -v `pwd`:$(DOCKER_PATH) -i -t $(DOCKER_DEVIMAGE) hack/make.sh cli
 
 check: build
 	@-docker rm -vf neosearch-ctn
--- hack/make.sh
+++ hack/make.sh
@@ -2,5 +2,44 @@
 
 ./hack/deps.sh
 
-go build -tags leveldb -v
-cd neosearch && go build -tags leveldb -v 
+VERSION=0.1.0
+
+DEFAULT_BUNDLES=(
+    library
+    server
+    cli
+)
+
+bundle() {
+	bundlescript=$1
+	bundle=$(basename $bundlescript)
+	echo "---> Making bundle: $bundle (in bundles/$VERSION/$bundle)"
+	mkdir -p bundles/$VERSION/$bundle
+	source "$bundlescript" "$(pwd)/bundles/$VERSION/$bundle"
+}
+
+main() {
+    # We want this to fail if the bundles already exist and cannot be removed.
+    # This is to avoid mixing bundles from different versions of the code.
+    mkdir -p bundles
+    if [ -e "bundles/$VERSION" ]; then
+	echo "bundles/$VERSION already exists. Removing."
+	rm -fr bundles/$VERSION && mkdir bundles/$VERSION || exit 1
+	echo
+    fi
+    SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+    if [ $# -lt 1 ]; then
+	bundles=(${DEFAULT_BUNDLES[@]})
+    else
+	bundles=($@)
+    fi
+
+    
+    for bundle in ${bundles[@]}; do
+	bundle $SCRIPTDIR/make/$bundle
+	echo
+    done
+}
+
+main "$@"
