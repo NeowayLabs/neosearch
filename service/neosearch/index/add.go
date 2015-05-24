4b53671ab80bbdba2a9306516f6ada1e9c3d13aa
Tiago Natel de Moura <tiago4orion@gmail.com>
2015-05-23 22:45:17 -0300

    added unit tests to  search. Improve the error handling of rest for add documents

--- service/neosearch/index/add.go
+++ service/neosearch/index/add.go
@@ -1,6 +1,7 @@
 package index
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -22,10 +23,18 @@ func NewAddHandler(search *neosearch.NeoSearch) *AddHandler {
 }
 
 func (handler *AddHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	var (
+		document []byte
+		err      error
+		exists   bool
+		docID    string
+		docIntID int
+	)
+
 	handler.ProcessVars(req)
 	indexName := handler.GetIndexName()
 
-	if exists, err := handler.search.IndexExists(indexName); exists != true && err == nil {
+	if exists, err = handler.search.IndexExists(indexName); exists != true && err == nil {
 		response := map[string]string{
 			"error": "Index '" + indexName + "' doesn't exists.",
 		}
@@ -39,31 +48,35 @@ func (handler *AddHandler) ServeHTTP(res http.ResponseWriter, req *http.Request)
 	}
 
 	if req.Method != "POST" {
-		res.WriteHeader(http.StatusBadRequest)
-		handler.Error(res, "Add document expect a POST request")
-		return
+		err = errors.New("Add document expect a POST request")
+		goto error_fatal
 	}
 
-	docID := handler.GetDocumentID()
-	docIntID, err := strconv.Atoi(docID)
-
-	document, err := ioutil.ReadAll(req.Body)
+	docID = handler.GetDocumentID()
+	docIntID, err = strconv.Atoi(docID)
 
 	if err != nil {
-		res.WriteHeader(http.StatusBadRequest)
-		handler.Error(res, err.Error())
-		return
+		goto error_fatal
+	}
+
+	document, err = ioutil.ReadAll(req.Body)
+
+	if err != nil {
+		goto error_fatal
 	}
 
 	err = handler.addDocument(indexName, uint64(docIntID), document)
 
+	handler.WriteJSON(res, []byte(fmt.Sprintf("{\"status\": \"Document %d indexed.\"}", docIntID)))
+
+	return
+
+error_fatal:
 	if err != nil {
 		res.WriteHeader(http.StatusBadRequest)
 		handler.Error(res, err.Error())
 		return
 	}
-
-	handler.WriteJSON(res, []byte(fmt.Sprintf("{\"status\": \"Document %d indexed.\"}", docIntID)))
 }
 
 func (handler *AddHandler) addDocument(indexName string, id uint64, document []byte) error {
---
+++ service/neosearch/index/search_test.go
@@ -0,0 +1,238 @@
+package index
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/NeowayLabs/neosearch/lib/neosearch"
+	"github.com/gorilla/mux"
+)
+
+func getSearchHandler() *SearchHandler {
+	cfg := neosearch.NewConfig()
+	cfg.Option(neosearch.DataDir("/tmp/"))
+	ns := neosearch.New(cfg)
+
+	handler := NewSearchHandler(ns)
+	return handler
+}
+
+func addDocumentsForSearch(indexName string) (*SearchHandler, error) {
+	handler := getSearchHandler()
+
+	ind, err := handler.search.CreateIndex(indexName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for i, doc := range []string{
+		`{"id": 0, "name": "Neoway Business Solution"}`,
+		`{"id": 1, "name": "Facebook Inc"}`,
+		`{"id": 2, "name": "Google Inc"}`,
+	} {
+
+		err = ind.Add(uint64(i), []byte(doc))
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return handler, nil
+}
+
+func TestSimpleSearch(t *testing.T) {
+	handler, err := addDocumentsForSearch("search-simple")
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	router := mux.NewRouter()
+	router.Handle("/{index}/_search", handler).Methods("POST")
+	ts := httptest.NewServer(router)
+
+	defer func() {
+		handler.search.DeleteIndex("search-simple")
+		ts.Close()
+		handler.search.Close()
+	}()
+
+	searchURL := ts.URL + "/search-simple/_search"
+
+	dsl := `
+        {
+            "from": 0,
+            "size": 10,
+            "query": {
+                "$and": [
+                    {"name": "neoway"}
+                ]
+            }
+        }`
+
+	req, err := http.NewRequest("POST", searchURL, bytes.NewBufferString(dsl))
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	content, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	resObj := map[string]interface{}{}
+
+	err = json.Unmarshal(content, &resObj)
+
+	if err != nil {
+		t.Error(err)
+		t.Errorf("Returned value: %s", string(content))
+		return
+	}
+
+	if resObj["error"] != nil {
+		t.Error(resObj["error"])
+		return
+	}
+
+	if resObj["total"] == nil {
+		t.Error("Invalid results response. Field 'total' is required.")
+		return
+	}
+
+	total, ok := resObj["total"].(float64)
+
+	if !ok {
+		t.Errorf("Total must be an float: %+v", total)
+		return
+	}
+
+	if int(total) != 1 {
+		t.Errorf("Search problem. Returns %d but the correct is %d", total, 1)
+		return
+	}
+}
+
+func TestSimpleANDSearch(t *testing.T) {
+	handler, err := addDocumentsForSearch("simple-and-search")
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	router := mux.NewRouter()
+	router.Handle("/{index}/_search", handler).Methods("POST")
+	ts := httptest.NewServer(router)
+
+	defer func() {
+		handler.search.DeleteIndex("simple-and-search")
+		ts.Close()
+		handler.search.Close()
+	}()
+
+	searchURL := ts.URL + "/simple-and-search/_search"
+
+	dsl := `
+        {
+            "from": 0,
+            "size": 10,
+            "query": {
+                "$and": [
+                    {"name": "inc"},
+                    {"name": "facebook"}
+                ]
+            }
+        }`
+
+	req, err := http.NewRequest("POST", searchURL, bytes.NewBufferString(dsl))
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	content, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	resObj := map[string]interface{}{}
+
+	err = json.Unmarshal(content, &resObj)
+
+	if err != nil {
+		t.Error(err)
+		t.Errorf("Returned value: %s", string(content))
+		return
+	}
+
+	if resObj["error"] != nil {
+		t.Error(resObj["error"])
+		return
+	}
+
+	if resObj["total"] == nil {
+		t.Error("Invalid results response. Field 'total' is required.")
+		return
+	}
+
+	total, ok := resObj["total"].(float64)
+
+	if !ok {
+		t.Errorf("Total must be an float: %+v", total)
+		return
+	}
+
+	if int(total) != 1 {
+		t.Errorf("Search problem. Returns %d but the correct is %d", total, 1)
+		return
+	}
+
+	results, ok := resObj["results"].([]interface{})
+
+	if !ok || results == nil {
+		t.Errorf("No results: %+v", resObj["results"])
+		return
+	}
+
+	if len(results) != 1 {
+		t.Errorf("Results length is invalid: %d", len(results))
+		return
+	}
+
+	r := results[0].(map[string]interface{})
+
+	if r["name"].(string) != "Facebook Inc" {
+		t.Errorf("Invalid result: %+v", r)
+	}
+}
