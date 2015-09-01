100a4563eb494cf3cf97818bfa9f26e5070467f5
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2015-09-01 09:26:21 -0300

    lib/neosearch/store/store_leveldb.go: fix debug info

--- lib/neosearch/store/store_leveldb.go
+++ lib/neosearch/store/store_leveldb.go
@@ -140,7 +140,7 @@ func (lvdb *LVDB) MergeSet(key []byte, value uint64) error {
 	}
 
 	if lvdb.Config.Debug {
-		fmt.Printf("[INFO] %d ids == %d GB of ids\n", len(data)/8, len(data)/(1000*1000*1000))
+		fmt.Printf("[INFO] %d ids == %d GB of ids\n", len(data)/8, len(data)/(1024*1024*1024))
 	}
 
 	buf = new(bytes.Buffer)
